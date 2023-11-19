8a09facbb6e45d1a75466b63435dc32e7cc009b8
adedhi <dadeshvir@gmail.com>
2023-11-18 23:34:06 -0500

    Created test.exe and updated print_matrix.c
    Created test.exe to test the printing of matrices (updated main.c with new matrices). Also updated print_matrix.c in an effort to control the output of elements that are otherwise too long and would throw the matrix out of allignment.

--- code/main.c
+++ code/main.c
@@ -6,6 +6,31 @@ int main(void){
     Matrix temp_matrix;
     temp_matrix.size = 2;
     initializeMatrix(temp_matrix);
+
+    for(int i = 0; i < temp_matrix.size; i++){
+        for(int j = 0; j < temp_matrix.size; j++){
+            temp_matrix.values[i][j] = (i + j);
+            temp_matrix.empty_indices[i][j] = 1;
+        }
+    }
+
+    Matrix temp_matrix_2;
+    temp_matrix_2.size = 3;
+    initializeMatrix(temp_matrix_2);
+
+    for(int i = 0; i < temp_matrix_2.size; i++){
+        for(int j = 0; j < temp_matrix_2.size; j++){
+            if(i == j){
+                continue;
+            }
+            temp_matrix_2.values[i][j] = (i + j);
+            temp_matrix_2.empty_indices[i][j] = 1;
+        }
+    }
+
+    temp_matrix.values[0][0] = 10.00;
+
     printMatrix(temp_matrix);
+    printMatrix(temp_matrix_2);
     return 0;
 }
--- code/print_matrix.c
+++ code/print_matrix.c
@@ -5,9 +5,9 @@ void printMatrix(Matrix matrix){
     for(int i = 0; i < matrix.size; i++){
         for(int j = 0; j < matrix.size; j++){
             if(matrix.empty_indices[i][j] == 1){
-                printf("%.2f\t", matrix.values[i][j]);
+                printf("%6.2f\t", matrix.values[i][j]);
             } else{
-                printf(" \t");
+                printf("   -   \t");
             }
         }
         printf("\n");
