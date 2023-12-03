7ef11b14bafc23e9b122088ceb7e66f2bf9a6574
adedhi <dadeshvir@gmail.com>
2023-12-02 22:20:32 -0500

    Update matrix_functions.c

--- code/matrix_functions.c
+++ code/matrix_functions.c
@@ -1,6 +1,6 @@
 #include "matrix.h"
 
-int setSize(Matrix *matrix, int size){
+int setSize(Matrix *matrix, int size){ // Requires a pointer to the matrix
     if((size == 2) || (size == 3) || (size == 4)){
         matrix->size = size;
         return(1);
@@ -9,7 +9,7 @@ int setSize(Matrix *matrix, int size){
     }
 }
 
-int setValue(Matrix *matrix, int i, int j, double value){
+int setValue(Matrix *matrix, int i, int j, double value){ // Requires a pointer to the matrix
     if(((0 <= i) && (i < getSize(*matrix))) && ((0 <= j) && (j < getSize(*matrix)))){
         matrix->values[i][j] = value;
         matrix->empty_indices[i][j] = 1;
